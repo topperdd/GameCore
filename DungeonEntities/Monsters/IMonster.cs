c794a15785ce89421a276eef11911f4048531b49
waldemar getmann <w.getmann@web.de>
2025-07-29 09:55:59 +0200

    TargetSelectionManager hinzugefügt

--- DungeonEntities/Monsters/IMonster.cs
+++ DungeonEntities/Monsters/IMonster.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GameCore.DungeonEntities
+namespace GameCore.DungeonEntities.Monsters
 {
     public interface IMonster
     {
